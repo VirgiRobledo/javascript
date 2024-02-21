9f40141b3ddfe7bc1a0c6eedeb09f609ae2bdecc
Virginia Robledo <virrobledo18@gmail.com>
2024-02-20 22:39:06 -0300

    pruebaaaa

---
+++ app.js
@@ -0,0 +1,50 @@
+
+// alert("Bienvenido a nuestro sitio web") //alerta que aparece al abrir la pag
+//VARIABLES: var,let y const
+
+var nombre = 'Virgi' ; //caja: nombre -> Almacena a Virgi STRING CADENA DE TEXTO Y TMB ALMANCENA NÚMEROS
+
+
+console.log(nombre)
+console.log(numero)
+
+nombre = "Samu"
+console.log(nombre)
+
+//SUMA
+var suma = 50 + 50;
+console.log(suma)
+//RESTA
+var resta = 15 - 10;
+console.log(resta);
+
+//MULTIPLICACION
+var multiplicacion= 2 * 7;
+console.log(multiplicacion);
+
+//DIVISION
+var  division = 20 / 2;
+console.log(division);
+
+//RESTO DE UNA DIVISION
+var restoDivision = 10 % 5;
+console.log(restoDivision)
+
+
+var texto = "Soy un texto";
+var numero = 25;
+
+//concatenacion
+var nombre = "Vir";
+var apellido = "Robledo";
+var edad = 17;
+
+console.log(`Hola soy ${nombre}, de apellido ${apellido} y tengo ${edad} años.`)
+
+if(edad >= 18){
+    console.log("Es  mayor de edad, puede pasar")
+}
+
+else(edad < 18){
+    console.log("No es mayor de edad, no puedes entrar")
+}
